baeb0a0ef3f53090922344af3745850683781f7e
seba <stinoco@fen.uchile.cl>
2021-04-08 20:34:36 -0400

    changed database url

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
